9de0af5cf8af87d1ffaccc321a633a7cc38c947e
Kostas Voulgaridis <kostas.voulgaridis@earthanalytics.no>
2021-02-24 15:43:20 +0200

    Init project & Classes

---
+++ src/com/company/User.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class User {
+    // Field (attributes)
+    public String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+    // Methods
+    public void sayHello() {
+        System.out.println("Hi, my name is " + this.name);
+    }
+}
