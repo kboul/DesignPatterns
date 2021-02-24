a7da6aa7fe1df0cfd928916cc3fd4a7d4f12d779
kboul <kos.voulgaridis@gmail.com>
2021-02-24 16:24:27 +0200

    Encapsulation

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,12 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        TaxCalculator calculator = getCalculator();
-        var tax = calculator.calculateTax();
-        System.out.println(tax);
-    }
-
-    public static TaxCalculator getCalculator() {
-        return new TaxCalculator2019();
+        var account = new Account();
+        account.deposit(10);
+        account.withdraw(5);
+        System.out.println(account.getBalance());
     }
 }
