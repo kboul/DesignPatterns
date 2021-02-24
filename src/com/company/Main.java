56cf07bbdb05c494d8510864125284ab421a27dc
Kostas Voulgaridis <kostas.voulgaridis@earthanalytics.no>
2021-02-24 15:57:09 +0200

    Coupling

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	    User user = new User("Mosh");
+	    User user = new User("Mosh",30);
         user.sayHello();
     }
 }
--- src/com/company/User.java
+++ src/com/company/User.java
@@ -4,11 +4,11 @@ public class User {
     // Field (attributes)
     public String name;
 
-    public User(String name) {
+    public User(String name, int age) {
         this.name = name;
     }
     // Methods
     public void sayHello() {
-        System.out.println("Hi, my name is " + this.name);
+        System.out.println("- Hi, my name is " + this.name);
     }
 }
