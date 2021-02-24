88f93847fae4ec812e437b8d63a967d9c748cbde
kboul <kos.voulgaridis@gmail.com>
2021-02-24 16:46:20 +0200

    Inheritance

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        var mailService = new MailService();
-        mailService.sendEmail();
+        var textBox = new TextBox();
+        textBox.enable();
     }
 }
