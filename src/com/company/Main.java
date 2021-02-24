b1201124626699883b82e17b8dafe2451c8b3d2d
kboul <kos.voulgaridis@gmail.com>
2021-02-24 16:37:51 +0200

    Abstraction

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,9 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        var account = new Account();
-        account.deposit(10);
-        account.withdraw(5);
-        System.out.println(account.getBalance());
+        var mailService = new MailService();
+        mailService.sendEmail();
     }
 }
